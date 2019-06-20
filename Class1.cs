5e3a056fc2e4940808d378a7c97d414f19c9437d
ione344444 <51847094+ione344444@users.noreply.github.com>
2019-06-20 11:16:34 +0800

    第一次提交

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CalculatorApplication
+{
+    class NullablesAtShow
+    {
+        static void Main(string[] args)
+        {
+            int? num1 = null;
+            int? num2 = 45;
+            double? num3 = new double?();
+            double? num4 = 3.14157;
+
+            bool? boolval = new bool?();
+
+            // 显示值
+
+            Console.WriteLine("显示可空类型的值： {0}, {1}, {2}, {3}",
+                               num1, num2, num3, num4);
+            Console.WriteLine("一个可空的布尔值： {0}", boolval);
+            Console.ReadLine();
+
+        }
+    }
+}
